c7a9794b8e6ee89272708bc0eae557ffa8282710
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2025-05-28 16:55:56 -0400

    Delete transformer_core_project/transformer_app_playground directory

--- transformer_core_project/transformer_app_playground/ui/sidebar.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def mostrar_sidebar():
-    total_params = st.session_state.get("total_params", None)
-    if total_params:
-        st.sidebar.markdown(f"**Parâmetros do modelo:** {total_params:,}")
