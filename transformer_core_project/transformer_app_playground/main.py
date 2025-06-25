dcb6e962e9ac96889a960d548ff13befc0b6274b
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2025-06-25 11:44:56 -0400

    Update main.py

--- transformer_core_project/transformer_app_playground/main.py
+++ transformer_core_project/transformer_app_playground/main.py
@@ -1,3 +1,4 @@
+#© 2025 Prof. Dr. Fabio Santos. Uso permitido apenas para fins educacionais e não comerciais.
 import streamlit as st
 from ui.sidebar import mostrar_sidebar
 from ui.layout import layout_principal
